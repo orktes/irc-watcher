c452dd8c080c365c74b6ffe3bf723e49891c4724
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2012-04-15 13:09:33 +0300

    Added surrounding messages query and image type for urls

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var app = express.createServer();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/messages/', function(req, res, next){
+app.get('/messages/', function(req, res, next) {
   var skip = req.query.skip || 0;
-  var limit =  req.query.limit || 20;
+  var limit = req.query.limit || 20;
 
   var search = req.query.s;
 
@@ -26,15 +26,15 @@ app.get('/messages/', function(req, res, next){
       if (err) {
         return next(err);
       }
-      
+
       res.send({data: {messages: data, search: search}, status: 'ok'});
     });
   }
 });
 
-app.get('/messages/special', function(req, res, next){
+app.get('/messages/special', function(req, res, next) {
   var skip = req.query.skip || 0;
-  var limit =  req.query.limit || 20;
+  var limit = req.query.limit || 20;
 
   var search = req.query.search;
 
@@ -51,9 +51,9 @@ app.get('/messages/special', function(req, res, next){
   }
 });
 
-app.get('/messages/tag/:tag', function(req, res, next){
+app.get('/messages/tag/:tag', function(req, res, next) {
   var skip = req.query.skip || 0;
-  var limit =  req.query.limit || 20;
+  var limit = req.query.limit || 20;
   var tag = req.params.tag;
 
   repositories.messages.getMessagesByTag(tag, skip, limit, function (err, data) {
@@ -65,9 +65,9 @@ app.get('/messages/tag/:tag', function(req, res, next){
   });
 });
 
-app.get('/messages/to/:to', function(req, res, next){
+app.get('/messages/to/:to', function(req, res, next) {
   var skip = req.query.skip || 0;
-  var limit =  req.query.limit || 20;
+  var limit = req.query.limit || 20;
   var to = req.params.to;
 
   repositories.messages.getMessagesByTo(to, skip, limit, function (err, data) {
@@ -79,9 +79,9 @@ app.get('/messages/to/:to', function(req, res, next){
   });
 });
 
-app.get('/messages/type/:type', function(req, res, next){
+app.get('/messages/type/:type', function(req, res, next) {
   var skip = req.query.skip || 0;
-  var limit =  req.query.limit || 20;
+  var limit = req.query.limit || 20;
   var type = req.params.type;
 
   repositories.messages.getMessagesByType(type, skip, limit, function (err, data) {
@@ -93,24 +93,59 @@ app.get('/messages/type/:type', function(req, res, next){
   });
 });
 
-app.get('/messages/:id', function(req, res, next){
+app.get('/messages/:id', function(req, res, next) {
   var id = req.params.id;
-  var baf = req.query.baf;
+  var before = req.query.before;
+  var after = req.query.after;
 
   repositories.messages.getMessage(id, function (err, data) {
     if (err) {
       return next(err);
     }
 
-    if (baf) {
-      // TODO LOAD MESSAGES BEFORE AND AFTER (BAF) THIS MESSAGE... TIME IS DEFINED IN THE baf variable
-    } else {
-      res.send({data: data, status: 'ok'});
+    if (data == null) {
+      return next(new Error("No message found"));
     }
+
+    res.send({data: data, status: 'ok'});
+
   });
 });
 
-app.get('/tags', function(req, res, next){
+app.get('/messages/:id/surrounding', function (req, res, next) {
+  var id = req.params.id;
+  var before = req.query.before;
+  var after = req.query.after;
+
+  before = Number(before || 0);
+  after = Number(after || 0);
+
+  if (before < 0 || after < 0) {
+    return next(new Error("Values should be positive"));
+  }
+
+  repositories.messages.getMessagesSurrounding(id, before, after, function (err, messages) {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    var after = [];
+    var before = [];
+
+    messages.forEach(function (msg) {
+      if (msg.id < id) {
+        before.push(msg);
+      } else if (msg.id > id) {
+        after.push(msg);
+      }
+    });
+
+    res.send({data: {id: id, after: after, before: before}, status: 'ok'});
+  });
+});
+
+app.get('/tags', function(req, res, next) {
   repositories.messages.getTags(function (err, data) {
     if (err) {
       return next(err);
@@ -120,7 +155,7 @@ app.get('/tags', function(req, res, next){
   });
 });
 
-app.get('/tos', function(req, res, next){
+app.get('/tos', function(req, res, next) {
   repositories.messages.getTos(function (err, data) {
     if (err) {
       return next(err);
@@ -130,7 +165,7 @@ app.get('/tos', function(req, res, next){
   });
 });
 
-app.get('/types', function(req, res, next){
+app.get('/types', function(req, res, next) {
   repositories.messages.getTypes(function (err, data) {
     if (err) {
       return next(err);
